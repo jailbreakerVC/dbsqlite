0129031be7b2d9db739d334f844e10f19f1d8e02
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2022-10-17 23:12:17 +0530

    files

---
+++ createDb.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `mall` (
+  `id` char PRIMARY KEY,
+  `veh_num` varchar(255),
+  `time_in` integer,
+  `time_out` integer,
+  `credit` integer,
+  `total_income` integer,
+  `date` date,
+  `veh_count` integer,
+  `location` char
+);
+
+CREATE TABLE `user` (
+  `user_id` char,
+  `time_in` integer,
+  `time_out` integer,
+  `amt_deducted` integer,
+  `phone_num` integer,
+  `location` char
+);
+
---
+++ database.js
@@ -0,0 +1,61 @@
+//DEPENDENCIES
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+//reading the file
+const dataSql = fs.readFileSync('createDb.sql').toString();
+console.log("file read")
+//connecting database
+// let db= new sqlite3.Database('testDB',sqlite3.OPEN_CREATE, (err)=>{
+//     if (err) {
+//      //    console.log(err.message);
+//         console.error("db_create:Err",{err})
+//     }
+//     console.log("Database suceesfully connected");
+// });
+let db= new sqlite3.Database('test5.db', (err)=>{
+     if (err) {
+         console.log(err.message);
+     }
+     else
+       console.log("Database suceesfully connected");
+ });
+//after read
+//console.log('after read')
+const dataArr=dataSql.toString().split(`;`)
+//const dataSql = fs.readFileSync('createDb.sql').toString();
+
+db.serialize(()=>{
+
+     console.log("serialising...")
+     
+    db.run('PRAGMA foreign_keys=ON;');
+    dataArr.forEach((query)=>{
+        if (query) {
+          //query=query.trim();
+           query=query.replace(/\r?\n|\r/g, " ");
+           console.log({query})
+            query+=`;`;
+            db.run(query, (err)=>{
+                if (err) {
+                    // onsole.log("in serialising")c
+                    //console.log(err.message);
+                    console.error("serialize:Err",{err})
+                }
+            });
+        }
+    });
+    console.log("not serialising error")
+//     db.run('COMMIT;');
+     //   db.commit();
+});
+
+db.run("INSERT INTO mall (id,time_in) VALUES(1,1400);");
+
+db.close((err)=>{
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Database Connection Closed');
+});
+
+
